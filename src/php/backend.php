e8a67d40eb039f4e79ec9fe7f804e795f3b2500e
NanoJoy <samuelevlafleur@gmail.com>
2017-03-26 15:52:11 -0400

    backend tweet getting works with username and search term

--- src/php/backend.php
+++ src/php/backend.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'TwitterAPIExchange.php';
+require_once 'libraries/TwitterAPIExchange.php';
 
 $settings = [
     'oauth_access_token' => '3302732766-tVsVY4btoBs6PCAJNoqnoxsimg7IytB13UF54nv',
@@ -8,13 +8,27 @@ $settings = [
     'consumer_secret' => 'Vxi3U14ndZXiYMmF6s3jyrzvHLTt0C98MSODJMIRtB2zbHlinD'
 ];
 
-$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+function getTweets($username = "", $tweetCount = 0, $search = "") {
 
-$requestMethod = "GET";
-$getField = '?screen_name=iagdotme&count=20';
+    $url = 'https://api.twitter.com/1.1/search/tweets.json';
 
-$twitter = new TwitterAPIExchange($settings);
-echo $twitter->setGetField($getField)
-             ->buildOauth($url, $requestMethod)
-             ->performRequest();
+    $requestMethod = "GET";
+    $getField = '?q=';
+    if ($username !== "") {
+        $getField = $getField . urlencode("from:" . $username);
+        if ($search !== "") {
+            $getField = $getField . urlencode(" " . $search);
+        }
+    } else if ($search !== "") {
+        $getField = $getField . urlencode($search);
+    }
+
+    $twitter = new TwitterAPIExchange($GLOBALS['settings']);
+    return $twitter->setGetField($getField)
+                 ->buildOauth($url, $requestMethod)
+                 ->performRequest();
+
+}
+
+echo getTweets('harinef', 1, "men");
 ?>
