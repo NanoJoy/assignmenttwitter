6f61e93386d52a3976c4e02a260545023497305c
NanoJoy <samuelevlafleur@gmail.com>
2017-03-26 18:22:29 -0400

    Location search added. Returns multiple error messages.

--- src/php/backend.php
+++ src/php/backend.php
@@ -8,7 +8,17 @@ $settings = [
     'consumer_secret' => 'Vxi3U14ndZXiYMmF6s3jyrzvHLTt0C98MSODJMIRtB2zbHlinD'
 ];
 
-function getTweets($username = "", $tweetCount = 0, $search = "") {
+function addError(&$errors, $message) {
+    $errors['error'] = true;
+    array_push($errors['messages'], $message); 
+}
+
+function getTweets($username = "", $search = "", $location = null, $tweetCount = 20) {
+
+    $errors = [
+        'error' => false,
+        'messages' => []
+    ];
 
     $url = 'https://api.twitter.com/1.1/search/tweets.json';
 
@@ -21,14 +31,57 @@ function getTweets($username = "", $tweetCount = 0, $search = "") {
         }
     } else if ($search !== "") {
         $getField = $getField . urlencode($search);
+    } else {
+        addError($errors, "Username and search field cannot both be blank");
+    }
+
+    if (is_array($location)) {
+        if (array_key_exists('latitude', $location) && is_numeric($location['latitude'])) {
+            $location['latitude'] = floatval($location['latitude']);
+            if ($location['latitude'] < -90 || $location['latitude'] > 90) {
+                addError($errors, "Latitude out of range");
+            }
+        } else {
+            addError($errors, "Latitude not valid");
+        }
+        if (array_key_exists('longitude', $location) && is_numeric($location['longitude'])) {
+            $location['longitude'] = floatval($location['longitude']);
+            if ($location['longitude'] < -180 || $location['longitude'] > 180) {
+                addError($errors, "Longitude out of range");
+            }
+        } else {
+            addError($errors, "Longitude not valid");
+        }
+        if (array_key_exists('radius', $location) && is_numeric($location['radius'])) {
+            $location['radius'] = intval($location['radius']);
+        } else {
+            $location['radius'] = 1;
+        }
+        if (!array_key_exists('unit', $location) || !($location['unit'] === 'mi' || $location['unit'] === 'km')) {
+            addError($errors, 'Units not valid');
+        }
+        $getField = $getField . '&geocode=' . urlencode($location['latitude'] . ',' . $location['longitude']
+                              . ',' . $location['radius'] . $location['unit']);
+    }
+
+    $getField = $getField . "&count=" . urlencode($tweetCount);
+
+    if ($errors['error']) {
+        return json_encode($errors);
     }
 
     $twitter = new TwitterAPIExchange($GLOBALS['settings']);
     return $twitter->setGetField($getField)
                  ->buildOauth($url, $requestMethod)
                  ->performRequest();
-
 }
 
-echo getTweets('harinef', 1, "men");
+$location = [
+    'latitude' => 41.3712,
+    'longitude' => -73.4140,
+    'radius' => 10,
+    'unit' => 'mi'
+];
+
+echo getTweets('harinef', '', $location, 5);
 ?>
