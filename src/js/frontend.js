9d5c7d83add8fabded6b52f60764fe0ae47cb2d7
NanoJoy <samuelevlafleur@gmail.com>
2017-03-26 22:25:28 -0400

    front end works with geolocation

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -3,6 +3,10 @@ $(function () {
     var searchTermsInput = $("#searchTerms");
     var tweetCountInput = $("#tweetCount");
     var resultsDiv = $("#resultsDiv");
+    var latitude = $("#latitude");
+    var longitude = $("#longitude");
+    var radius = $("#radius");
+    var unit = $("#unit");
 
     //Check if screen name exists as user types it in
     screenNameInput.keyup(function (e) {
@@ -23,35 +27,112 @@ $(function () {
         });
     });
 
+    $("#useMyLocation").click(function (e) {
+        function positionGotten(position) {
+            latitude.val(position.coords.latitude);
+            longitude.val(position.coords.longitude);
+        }
+
+        if (this.checked) {
+            if (navigator.geolocation) {
+                latitude.prop("disabled", true);
+                longitude.prop("disabled", true);
+                navigator.geolocation.getCurrentPosition(positionGotten);
+            } else {
+                $(this).prop("disabled", true);
+                $("label [for='useMyLocation']").html("Geolocation not supported by this browser");
+            }
+        } else {
+            latitude.prop("disabled", false);
+            longitude.prop("disabled", false);
+
+        }
+    });
+
+    $("#latitude").keyup(function (e) {
+        var value = parseInt($(this).val(), 10);
+        if (value < -90) {
+            $(this).val(-90);
+        }
+        if (value > 90) {
+            $(this).val(90);
+        }
+    });
+
+    $("#longitude").keyup(function (e) {
+        var value = parseInt($(this).val(), 10);
+        if (value < -180) {
+            $(this).val(-180);
+        }
+        if (value > 180) {
+            $(this).val(180);
+        }
+    });
+
+    $("#usingLocation").change(function (e) {
+        $("#locationInputs").toggle(this.checked);
+    }).change();
+
     $("#submitButton").click(function (e) {
-        $("#searchInputsDiv :input").prop("disabled", true);
+        $("#errors").empty();
+        resultsDiv.empty();
+        var makeSearch = true;
         var data = {
             screenName: screenNameInput.val(),
             searchTerms: searchTermsInput.val(),
             tweetCount: tweetCountInput.val()
         };
-        $.ajax({
-            url: "src/php/getTweets.php",
-            data: data,
-            success: function (data) {
-                $("#searchInputsDiv :input").prop("disabled", false);
-                resultsDiv.empty();
-                $("#errors").empty();
-                if (data.error) {
-                    for (var i = 0; i < data.messages.length; i++) {
-                        $("#errors").append("<p>" + data.messages[i] + "</p>");
+
+        if ($("#usingLocation").is(":checked")) {
+            if (latitude.val() === "") {
+                makeSearch = false;
+                $("#errors").append("<p>Please enter a latitude</p>")
+            }
+            if (longitude.val() === "") {
+                makeSearch = false;
+                $("#errors").append("<p>Please enter a longitude</p>")
+            }
+            if (latitude.val() === "") {
+                makeSearch = false;
+                $("#errors").append("<p>Please enter a radius</p>")
+            }
+            data.location = {
+                latitude: latitude.val(),
+                longitude: longitude.val(),
+                radius: radius.val(),
+                unit: unit.val()
+            }
+        }
+
+        if (makeSearch) {
+            $("#searchInputsDiv :input").prop("disabled", true);
+            $.ajax({
+                url: "src/php/getTweets.php",
+                data: data,
+                error: function (a, b, c) {
+                    console.log(a);
+                },
+                success: function (data) {
+                    $("#searchInputsDiv :input").prop("disabled", false);
+                    if ($("#useMyLocation").is(":checked")) {
+                        $("#latitude, #longitude").prop("disabled", true);
                     }
-                    return;
-                }
-                var tweets = data.statuses;
-                if (tweets.length === 0) {
-                    resultsDiv.append("<p>No tweets found matching search</p>");
-                }
-                for (var i = 0; i < tweets.length; i++) {
-                    resultsDiv.append("<p>" + tweets[i].text + "</p>");
-                }
-            },
-            dataType: "json"
-        });
+                    if (data.error) {
+                        for (var i = 0; i < data.messages.length; i++) {
+                            $("#errors").append("<p>" + data.messages[i] + "</p>");
+                        }
+                        return;
+                    }
+                    var tweets = data.statuses;
+                    if (tweets.length === 0) {
+                        resultsDiv.append("<p>No tweets found matching search</p>");
+                    }
+                    for (var i = 0; i < tweets.length; i++) {
+                        resultsDiv.append("<p>" + tweets[i].text + "</p>");
+                    }
+                },
+                dataType: "json"
+            });
+        }
     });
 });
--- src/php/getTweets.php
+++ src/php/getTweets.php
@@ -31,32 +31,32 @@ function getTweets($username = "", $search = "", $location = null, $tweetCount =
 
     if (is_array($location)) {
         if (array_key_exists('latitude', $location) && is_numeric($location['latitude'])) {
-            $location['latitude'] = floatval($location['latitude']);
+            /*$location['latitude'] = floatval($location['latitude']);
             if ($location['latitude'] < -90 || $location['latitude'] > 90) {
                 addError($errors, "Latitude out of range");
-            }
+            }*/
         } else {
             addError($errors, "Latitude not valid");
         }
         if (array_key_exists('longitude', $location) && is_numeric($location['longitude'])) {
-            $location['longitude'] = floatval($location['longitude']);
+           /* $location['longitude'] = floatval($location['longitude']);
             if ($location['longitude'] < -180 || $location['longitude'] > 180) {
                 addError($errors, "Longitude out of range");
-            }
+            }*/
         } else {
             addError($errors, "Longitude not valid");
         }
         if (array_key_exists('radius', $location) && is_numeric($location['radius'])) {
-            $location['radius'] = intval($location['radius']);
+            //$location['radius'] = intval($location['radius']);
         } else {
             $location['radius'] = 1;
         }
         if (!array_key_exists('unit', $location) || !($location['unit'] === 'mi' || $location['unit'] === 'km')) {
             addError($errors, 'Units not valid');
         }
-        $getField = $getField . '&geocode=' . urlencode($location['latitude'] . ',' . $location['longitude']
-                              . ',' . $location['radius'] . $location['unit']);
+        $getField = $getField . '&geocode=' . $location['latitude'] . ',' . $location['longitude'] . ',' . $location['radius'] . $location['unit'];
     }
+    //$getField = $getField . '&geocode=' . 48.8566 . ',' . 2.3522 . ',' . 10 . 'mi';
 
     $getField = $getField . "&count=" . urlencode($tweetCount);
 
@@ -70,16 +70,10 @@ function getTweets($username = "", $search = "", $location = null, $tweetCount =
                  ->performRequest();
 }
 
-$location = [
-    'latitude' => 41.3712,
-    'longitude' => -73.4140,
-    'radius' => 10,
-    'unit' => 'mi'
-];
-
 $screenName = $_GET['screenName'];
 $searchTerms = $_GET['searchTerms'];
 $tweetCount = intval($_GET['tweetCount']);
+$location = $_GET['location'];
 
-echo getTweets($screenName, $searchTerms, null, $tweetCount);
+echo getTweets($screenName, $searchTerms, $location, $tweetCount);
 ?>
